b437028d4939752e17a611ded0abc920ecd273bf
ifwonders <793879706@qq.com>
2024-06-13 23:14:19 +0800

    使用了集成模型 提升了预测准确率

--- bin/best_classification_model.py
+++ bin/best_classification_model.py
@@ -1,9 +1,12 @@
 import joblib
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
 from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
 
 from bin.data_preprocess import text_preprocess, data_sets_preprocess
+from feature_extractions.bag_of_words import BoW
 
 # 文件路径
 text_path = 'bin/data_sets/mr.txt'
@@ -27,15 +30,47 @@ class classification_model:
             data_sets_preprocess(text_path, label_path)
         )
 
-        # 选择特征提取方法 以TF-IDF为例
-        tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
-        # 选择模型 以SVM为例
-        svm_classifier = SVC(kernel='linear', C=1.0)
+        # 选择特征提取方法 选用BoW方法
+        bow_vectorizer = CountVectorizer()
+        train_vectors, test_vectors = BoW(texts_train, texts_test)
+
+        # 创建多个单独的模型
+        svm_classifier = SVC(C=1.0, coef0=1.0, degree=2, gamma='scale', kernel='poly', probability=True)
+        # dt_classifier = DecisionTreeClassifier(criterion='entropy',max_features='sqrt',min_samples_leaf=2,min_samples_split=2,splitter='random')
+        rf_classifier = RandomForestClassifier(criterion='gini', max_depth=4, max_features='sqrt', min_samples_leaf=2,
+                                               min_samples_split=2)
+
+        # 训练模型
+        svm_classifier.fit(train_vectors, train_labels_list)
+        # dt_classifier.fit(train_vectors, train_labels_list)
+        rf_classifier.fit(train_vectors, train_labels_list)
+
+        # 创建一个投票集成模型
+        ensemble_model = VotingClassifier(estimators=[
+            ('svm', svm_classifier),
+            # ('dt', dt_classifier),
+            ('rf', rf_classifier)],
+            voting='soft',
+            weights=[0.75,
+                     # 0.67,
+                     0.72]
+        )
+
+        # 训练集成模型
+        ensemble_model.fit(train_vectors, train_labels_list)
+
         # 使用make_pipeline函数创建流水线 串联向量化器和NLP模型
-        self.model = make_pipeline(tfidf_vectorizer, svm_classifier)
+        self.model = make_pipeline(bow_vectorizer, ensemble_model)
 
         # 训练模型
         self.model.fit(texts_train, train_labels_list)
+
+        # 使用模型进行预测
+        prediction = self.model.predict(texts_test)
+
+        # 评估模型
+        accuracy = accuracy_score(test_labels_list, prediction)
+        print(f"集成模型的准确率: {accuracy * 100:.2f}%")
 
         # 保存模型
         joblib.dump(self.model, model_save_path)
@@ -56,5 +91,12 @@ class classification_model:
 
 
 if __name__ == '__main__':
+    # 文件路径
+    text_path = 'data_sets/mr.txt'
+    # 标签路径
+    label_path = 'data_sets/mr_labels.txt'
+    # 训练模型保存路径
+    model_save_path = 'text_classifier.joblib'
+
     my_model = classification_model()
     my_model.predict_new_data('an ugly-duckling tale so hideously and clumsily told it feels accidental . ')
